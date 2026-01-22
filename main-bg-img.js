d4211bb6b75c18523d32820e91b2d65663ee48d8
alexwifi64 <alexlopnog@gmail.com>
2026-01-22 18:18:35 +0100

    img caroussel, centered logo

--- main-bg-img.js
+++ main-bg-img.js
@@ -1,14 +1,32 @@
-let backgroundDiv = document.querySelector("#background");
-let imgIndex = getRandomInt(22) + 1
+let backgroundDiv = document.querySelector(".background");
 
-
-function randomBg() {
-	backgroundDiv.style.backgroundImage = `url(img/bg-imgs/${imgIndex}.webp)`;
-}
-
+let imgIndex = 0
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
-}
+};
 
+function randomiseIndex(){
+	imgIndex = getRandomInt(17) + 1
+};
+
+const randomBg = async () => {
+	backgroundDiv.classList.remove("background-active");
+	setTimeout(() => {
+		backgroundDiv.style.backgroundImage = `url(img/bg-imgs/${imgIndex}.webp)`;
+		// backgroundDiv.classList.add("background-active");
+	}, "500");
+	setTimeout(() => {
+		// backgroundDiv.style.backgroundImage = `url(img/bg-imgs/${imgIndex}.webp)`;
+		backgroundDiv.classList.add("background-active");
+	}, "500");
+};
+
+setInterval(() => {
+	randomiseIndex();
+	randomBg();
+}, "5000");
+
+
+randomiseIndex();
 randomBg();
